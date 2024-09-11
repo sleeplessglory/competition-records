99b1fa8f61a5c27c3ea05b4fc4e077e4195c4aeb
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2024-09-11 14:32:38 +0300

    Ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/competition-records/",
   plugins: [react()],
   server: { //the next 3 lines need to be added in order to launch it on mobile devices
     host: true  
